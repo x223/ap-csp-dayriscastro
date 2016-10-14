5060768dfe730b6937d89e4e095db9f44e647a37
dayriscastro <dayris.castro@ms223.org>
2016-10-14 14:29:17 -0400

    Add files via upload

---
+++ Traffic light/sketch.js
@@ -0,0 +1,17 @@
+function setup() { 
+  createCanvas(400, 400);
+}
+
+function draw() { 
+  background('white');
+	
+	fill('black')
+rect(100,130,100,200)
+	fill('white')
+ellipse(150,165,45,45)
+ellipse(150,225,45,45)
+ellipse(150,285,45,45)
+	fill('grey')
+rect(140,330,20,410)
+}
+
